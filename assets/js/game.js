fec051e233feb32ceb72e55e7c01dcc3d9a48b34
Mystic Kitty <djabranton@gmail.com>
2020-09-28 18:13:26 -0700

    Fixed Round Number Notification

--- assets/js/game.js
+++ assets/js/game.js
@@ -71,11 +71,21 @@ var fight = function(enemyName) {
     }
   };
 
+
+
   // execute function
   for (var i = 0; i < enemyNames.length; i++) {
-    var pickedEnemyName = enemyNames[i];
-    enemyHealth = 50;
-    fight(pickedEnemyName);
+    if (playerHealth > 0) {
+        window.alert("Welcome to Robot Gladiators! Round " + ( i + 1 ) );
+        var pickedEnemyName = enemyNames[i];
+        enemyHealth = 50;
+        fight(pickedEnemyName);
+      }
+      else {
+        window.alert("You have lost your robot in battle! Game Over!");
+        break;
+      }
+
 }
 
 
