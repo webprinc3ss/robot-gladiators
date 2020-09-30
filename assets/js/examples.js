ff45426b8ddf4f33e807b123c4019913734f2d5d
Mystic Kitty <djabranton@gmail.com>
2020-09-29 17:01:28 -0700

    random health and attack values

---
+++ assets/js/examples.js
@@ -0,0 +1,17 @@
+// prints 3.141592653589793
+console.log(Math.PI);
+
+ // rounds to the nearest whole number (4)
+console.log(Math.round(4.4));
+
+ // prints the square root (5)
+console.log(Math.sqrt(25));
+
+// prints 100
+console.log(Math.max(10, 20, 100));
+
+// prints 0
+console.log(Math.max(0, -50));
+
+
+Math.random()
