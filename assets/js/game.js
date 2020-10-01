122e178ea3f1e6ae8c61309c099aad2739f70d27
Mystic Kitty <djabranton@gmail.com>
2020-10-01 08:21:40 -0700

    Integer number fix replaced shop choices.

--- assets/js/game.js
+++ assets/js/game.js
@@ -162,27 +162,26 @@ window.alert("Thank you for playing Robot Gladiators! Come back soon!");
 var shop = function() {
 // ask player what they'd like to do
 var shopOptionPrompt = window.prompt(
-  "Would you like to REFILL your health, UPGRADE your attack, or LEAVE the store? Please enter one: 'REFILL', 'UPGRADE', or 'LEAVE' to make a choice."
+  "Would you like to REFILL your health, UPGRADE your attack, or LEAVE the store? Please enter one 1 for REFILL, 2 for UPGRADE, or 3 for LEAVE."
 );
 // use switch to carry out action
+shopOptionPrompt = parseInt(shopOptionPrompt);
+debugger;
 switch (shopOptionPrompt) {
-case "REFILL":
-case "refill":
-playerInfo.refillHealth();
-break;
-case "UPGRADE":
-case "upgrade":
-playerInfo.upgradeAttack();
-break;
-case "LEAVE": // new case
-case "leave":
-  window.alert("Leaving the store.");
+    case 1:
+      playerInfo.refillHealth();
   break;
-default:
-  window.alert("You did not pick a valid option. Try again.");
-  shop();
+    case 2:
+      playerInfo.upgradeAttack();
   break;
-}
+    case 3:
+      window.alert("Leaving the store.");
+  break;
+    default:
+      window.alert("You did not pick a valid option. Try again.");
+      shop();
+  break;
+  }
 };
 
 // function to generate a random numeric value
