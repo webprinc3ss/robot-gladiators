a5c9f128699ec8a70b4f71defad65fdac1ae2cde
Mystic Kitty <djabranton@gmail.com>
2020-10-01 15:58:36 -0700

    Added random robot fighting order

--- assets/js/game.js
+++ assets/js/game.js
@@ -48,28 +48,45 @@ promptFight = promptFight.toLowerCase();
     }
   }
 
-var fight = function(enemy) {
-    // repeat and execute as long as the enemy robot is alive 
-    while (playerInfo.health > 0 && enemy.health > 0) {
-    // ask user if they'd like to fight or skip using fightOrSkip function
-    if (fightOrSkip()) {
-      // if true, leave fight by breaking loop
-      break;
-    }
-    // var damage = randomNumber(playerInfo.attack - 3, playerInfo.attack);
-  
-    // remove enemy's health by subtracting the amount set in the playerInfo.attack variable
-    // generate random damage value based on player's attack power
-    var damage = randomNumber(playerInfo.attack - 3, playerInfo.attack);
 
-    enemy.health = Math.max(0, enemy.health - damage);
-    console.log(
-      playerInfo.name + ' attacked ' + enemy.name + '. ' + enemy.name + ' now has ' + enemy.health + ' health remaining.'
-    );
+  
+
+//FIGHT FUNCTION
+var fight = function(enemy) {
+  // keep track of who goes first
+  var isPlayerTurn = true;
+
+  // randomly change turn order
+  if (Math.random() > 0.5) {
+    isPlayerTurn = false;
+  }
+
+  while (playerInfo.health > 0 && enemy.health > 0) {
+    if (isPlayerTurn) {
+      // ask user if they'd like to fight or skip using fightOrSkip function
+      if (fightOrSkip()) {
+        // if true, leave fight by breaking loop
+        break;
+      }
+  
+  var damage = randomNumber(playerInfo.attack - 3, playerInfo.attack);
+
+// remove enemy's health by subtracting the amount we set in the damage variable
+enemy.health = Math.max(0, enemy.health - damage);
+console.log(
+  playerInfo.name +
+    " attacked " +
+    enemy.name +
+    ". " +
+    enemy.name +
+    " now has " +
+    enemy.health +
+    " health remaining."
+);
 
     // check enemy's health
     if (enemy.health <= 0) {
-      window.alert(enemy.name + ' has died!');
+      window.alert(enemy.name + " has died!");
 
       // award player money for winning
       playerInfo.money = playerInfo.money + 20;
@@ -77,25 +94,37 @@ var fight = function(enemy) {
       // leave while() loop since enemy is dead
       break;
     } else {
-      window.alert(enemy.name + ' still has ' + enemy.health + ' health left.');
+      window.alert(enemy.name + " still has " + enemy.health + " health left.");
     }
 
-    // remove players's health by subtracting the amount set in the enemy.attack variable
+    // player gets attacked first
+  } else {
     var damage = randomNumber(enemy.attack - 3, enemy.attack);
 
+    // remove enemy's health by subtracting the amount we set in the damage variable
     playerInfo.health = Math.max(0, playerInfo.health - damage);
     console.log(
-      enemy.name + ' attacked ' + playerInfo.name + '. ' + playerInfo.name + ' now has ' + playerInfo.health + ' health remaining.'
+      enemy.name +
+        " attacked " +
+        playerInfo.name +
+        ". " +
+        playerInfo.name +
+        " now has " +
+        playerInfo.health +
+        " health remaining."
     );
 
     // check player's health
     if (playerInfo.health <= 0) {
-      window.alert(playerInfo.name + ' has died!');
+      window.alert(playerInfo.name + " has died!");
       // leave while() loop if player is dead
       break;
     } else {
-      window.alert(playerInfo.name + ' still has ' + playerInfo.health + ' health left.');
+      window.alert(playerInfo.name + " still has " + playerInfo.health + " health left.");
     }
+  }
+  // switch turn order for next round
+  isPlayerTurn = !isPlayerTurn;
   }
 };
 
@@ -108,7 +137,6 @@ var startGame = function() {
   for (var i = 0; i < enemyInfo.length; i++) {
     if (playerInfo.health > 0) {
       window.alert("Welcome to Robot Gladiators! Round " + (i + 1));
-      debugger;
 
       var pickedEnemyObj = enemyInfo[i];
 
@@ -166,7 +194,6 @@ var shopOptionPrompt = window.prompt(
 );
 // use switch to carry out action
 shopOptionPrompt = parseInt(shopOptionPrompt);
-debugger;
 switch (shopOptionPrompt) {
     case 1:
       playerInfo.refillHealth();
